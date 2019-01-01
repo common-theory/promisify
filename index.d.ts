e3fe306f9e2e02a45037e75155508d817a7731f1
Chance Hudson <jchancehud@gmail.com>
2018-12-31 19:13:03 -0600

    Export function as default

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,5 @@ type CallbackFunction<T, R> = (...args: T[]) => R;
 type PromiseFunction<T, R> = (...args: T[]) => Promise<R>
 
 declare module '@ctheory/promisify' {
-  function promisify<T, R>(fn: CallbackFunction<T, R>): PromiseFunction<T, R>;
+  export default function promisify<T, R>(fn: CallbackFunction<T, R>): PromiseFunction<T, R>;
 }
