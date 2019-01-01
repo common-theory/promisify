b7fd595e39f379e899aa6434bdb713c5119b24bb
Chance Hudson <jchancehud@gmail.com>
2018-12-31 19:12:49 -0600

    Refactor promisify typedef

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,6 @@
+type CallbackFunction<T, R> = (...args: T[]) => R;
+type PromiseFunction<T, R> = (...args: T[]) => Promise<R>
+
 declare module '@ctheory/promisify' {
-  function promisify<R, A>(fn: (...A) => R): (...A) => Promise<R>;
+  function promisify<T, R>(fn: CallbackFunction<T, R>): PromiseFunction<T, R>;
 }
